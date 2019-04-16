d837857f4916180fc35b3eb1ba42e15138640aa7
Justice <popystone@gmail.cm>
2019-04-16 22:25:11 +0100

    Added Home contract view override methods to the home activity

--- app/src/main/java/com/pixelart/cvappchallenge/ui/homescreen/HomeActivity.kt
+++ app/src/main/java/com/pixelart/cvappchallenge/ui/homescreen/HomeActivity.kt
@@ -2,12 +2,26 @@ package com.pixelart.cvappchallenge.ui.homescreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pixelart.cvappchallenge.R
 
-class HomeActivity : AppCompatActivity() {
-
+class HomeActivity : AppCompatActivity(), HomeContract.View {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+    
+    override fun showCVDetail(name: String, phoneEmail: String, experienceSummary: String,
+                              techSkills: String, workHistory: String, education: String, interest: String) {
+        
+    }
+    
+    override fun showError(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+    
+    override fun showMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
